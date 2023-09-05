c87bdb5e249767b91a6e4bb0c694bd73d60b9471
TheWanderingShinobi <thewanderingshinobi@proton.me>
2023-09-05 14:33:46 -0400

    working on Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,14 @@
 import avatar from "/avatar.png"
 import { motion } from 'framer-motion'
 import { useState } from "react"
+import { useMediaQuery } from "../util/useMediaQuery"
 
 
 
 export default function Nav() {
   const [toggled, setToggled] = useState(false)
+
+  const matches = useMediaQuery("(min-width: 1280px)")
 
   return (
     <nav className="relative mx-8 mb-24 flex justify-between items-center pt-12 pb-6 font-medium md:mx-16 lg:mx-32">
@@ -18,19 +21,29 @@ export default function Nav() {
         <path d="M2 2L428 2" strokeWidth={2} stroke="#282828" strokeLinecap="round" />
       </svg>
       <div>
-        <img src={avatar} alt="Project Logo" />
+        <img src={avatar} alt="Hua profile picture" />
       </div>
 
       {/*Title*/}
+
       <h1 className="text-lg font-bold">
         <a href="/"> SelfhostLab.</a>
       </h1>
-      <div onClick={() => setToggled((prevToggle) => !prevToggle)} className="space-y-1 cursor-pointer xl:hidden">
-        <span className="block h-0.5 w-8 bg-black"></span>
-        <span className="block h-0.5 w-6 bg-black"></span>
-        <span className="block h-0.5 w-4 bg-black"></span>
+      {!matches && (
+  )}
 
-      </div>
+
+      {!matches && (
+
+        <div onClick={() => setToggled((prevToggle) => !prevToggle)} className="space-y-1 cursor-pointer">
+          <span className="block h-0.5 w-8 bg-black"></span>
+          <span className="block h-0.5 w-6 bg-black"></span>
+          <span className="block h-0.5 w-4 bg-black"></span>
+
+        </div>
+      )}
+
+
     </nav>
   )
 }
